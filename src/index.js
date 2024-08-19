89c143c82c00d9b43c8d969873ba45007f3c005d
exoream <shiigetsu1@gmail.com>
2024-08-19 13:30:48 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ expressWs(app);
 // Array to keep track of connected WebSocket clients
 const clients = new Map();
 
+app.use(cors());
 app.use(express.json());
 app.use("/", routes);
-app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({
